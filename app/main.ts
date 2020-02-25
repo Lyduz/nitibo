00500866cb110ffe56a371a15d1c047e797e2187
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2020-02-25 17:00:19 +0800

    configure main typescript file for breaking changes

--- app/main.ts
+++ app/main.ts
@@ -1,16 +1,23 @@
-import Vue from 'nativescript-vue';
-import router from './router';
-import store from './store';
-import './styles.scss';
+import Vue from "nativescript-vue";
+import VueDevtools from "nativescript-vue-devtools";
+import Navigator from "nativescript-vue-navigator";
 
-// See NativeScript-Vue output logs in debug
-Vue.config.silent = !DEBUG_MODE
+import { routes } from "./router";
+import store from "./store";
+
+import App from "./App.vue";
+import "./styles.scss";
+
+Vue.use(Navigator, { routes });
+
+if (TNS_ENV !== "production") {
+  //Vue.use(VueDevtools);
+}
+
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = TNS_ENV === "production";
 
 new Vue({
-
-  router,
-  template: `<Frame><router-view/></Frame>`,
-
   store,
-
+  render: h => h(App)
 }).$start();
