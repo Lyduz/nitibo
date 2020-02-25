dd0f9f1738f07aa8e66a679bdb13a0e06f4686ce
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2020-02-25 16:58:44 +0800

    drop vue-router support

--- app/router/index.ts
+++ app/router/index.ts
@@ -1,33 +1,17 @@
-import Vue from 'nativescript-vue'
-import VueRouter from 'vue-router'
+import Home from "../components/Home.vue";
+import Categories from "../components/Categories.vue";
 
-Vue.use(VueRouter);
-
-import Home from '../components/Home.vue'
-import Categories from '../components/Categories.vue'
-
-const router = new VueRouter({
-  routes: [
-    {
-      path: '/home',
-      component: Home,
-      meta: {
-        title: 'Home',
-      },
-    },
-    {
-      path: '/categories',
-      component: Categories,
-      meta: {
-        title: 'Categories',
-      },
-    },
-
-    //default
-    { path: '*', redirect: '/home' },
-  ],
-});
-
-router.replace('/home');
-
-export default router;
+export const routes = {
+  '/home': {
+    component: Home,
+    meta: {
+      title: "Home"
+    }
+  },
+  '/categories': {
+    component: Categories,
+    meta: {
+      title: "Categories"
+    }
+  },
+}
