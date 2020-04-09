82551d73cf19bcbc8bc65848b04933a7409a2c87
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2020-04-09 13:32:43 +0800

    refactor for more explicit type return type

--- app/modules/social/store/getters.ts
+++ app/modules/social/store/getters.ts
@@ -1,7 +1,7 @@
 import { GetterTree } from 'vuex'
 import { State } from './state'
 
-export const Getters: GetterTree<typeof State, any> = {
+export const Getters: GetterTree<typeof State, () => any> = {
   getTitle(State): string {
     return State.title
   },
