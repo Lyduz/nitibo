38f3df5792146b1787acfe7e67d853cedf5cec50
Karl Anthony Baluyot <sovietkamarov@gmail.com>
2020-04-13 15:08:32 +0800

    fix vue store not appearing in vue dev tools

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,10 @@
 import Vue from 'nativescript-vue'
 import { VNode } from 'vue/types/umd'
+import VueDevtools from 'nativescript-vue-devtools'
+
+if (TNS_ENV !== 'production') {
+  Vue.use(VueDevtools)
+}
 
 // global dependency
 import routes from './router'
@@ -9,17 +14,13 @@ import store from './store'
 import App from './App.vue'
 import './styles.scss'
 import Navigator from 'nativescript-vue-navigator'
-import VueDevtools from 'nativescript-vue-devtools'
 
 // register global components
 Vue.use(Navigator, { routes })
 
-if (TNS_ENV !== 'production') {
-  Vue.use(VueDevtools)
-}
-
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = TNS_ENV === 'production'
+Vue.config.devtools = TNS_ENV === 'development'
 
 new Vue({
   store,
